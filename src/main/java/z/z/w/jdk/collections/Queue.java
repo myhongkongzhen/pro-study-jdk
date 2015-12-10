8cd3a59d6795043aacdff1b4a107376cea27a7f2
Z_Z.W <myhongkongzhen@gmail.com>
2015-12-10 14:42:24 +0800

    Collections -- Queue 相關 【由於LinkedList引用了Deque，所以先學習Queue相關

--- src/main/java/z/z/w/jdk/collections/Queue.java
+++ src/main/java/z/z/w/jdk/collections/Queue.java
@@ -13,14 +13,22 @@ package z.z.w.jdk.collections;
 
 /**
  * A collection designed for holding elements prior to processing.
+ * 一個集合設計成為維護元素進行的先驅順序
  * Besides basic {@link java.util.Collection Collection} operations,
+ * 包含基本的集合操作
  * queues provide additional insertion, extraction, and inspection
+ * 隊列提供增加的插入，提取，檢查操作
  * operations.  Each of these methods exists in two forms: one throws
+ *              每個方法都存在兩個形式:
  * an exception if the operation fails, the other returns a special
+ * 如果操作失敗，拋出一個異常,
  * value (either <tt>null</tt> or <tt>false</tt>, depending on the
+ *  另外的返回一個特定的值null或者fasle
  * operation).  The latter form of the insert operation is designed
+ * 依賴於操作.    插入操作最新的形式是特別設計成為容量限定的queue的實現
  * specifically for use with capacity-restricted <tt>Queue</tt>
  * implementations; in most implementations, insert operations cannot
+ *                  大多數實現，插入操作不能失敗
  * fail.
  *
  * <p>
@@ -48,56 +56,87 @@ package z.z.w.jdk.collections;
  * </table>
  *
  * <p>Queues typically, but do not necessarily, order elements in a
+ * 一般的隊列，但不是必要的，元素按照fifo方式排列
  * FIFO (first-in-first-out) manner.  Among the exceptions are
  * priority queues, which order elements according to a supplied
+ * 在異常中優先的隊列,按照提供的比較器排列元素順序
  * comparator, or the elements' natural ordering, and LIFO queues (or
+ *             或者按照元素自然順序排列
  * stacks) which order the elements LIFO (last-in-first-out).
+ * lifo的隊列(或者棧)按照lifo順序排列元素
  * Whatever the ordering used, the <em>head</em> of the queue is that
+ * 無論使用哪種順序,             隊列的頭是調用remove或者poll方法移除的元素
  * element which would be removed by a call to {@link #remove() } or
  * {@link #poll()}.  In a FIFO queue, all new elements are inserted at
+ *                   在一個fifo隊列中,所有新元素添加在隊列的末尾
  * the <em> tail</em> of the queue. Other kinds of queues may use
  * different placement rules.  Every <tt>Queue</tt> implementation
+ * 另一些類型的queue有不同的放置規則
  * must specify its ordering properties.
+ * 每一個queue實現都必須指定他的排序屬性
  *
  * <p>The {@link #offer offer} method inserts an element if possible,
+ * 如果可以offer方法插入一個元素
  * otherwise returning <tt>false</tt>.  This differs from the {@link
  * java.util.Collection#add Collection.add} method, which can fail to
+ * 否則返回false                         與Collection.add方法不同，
  * add an element only by throwing an unchecked exception.  The
+ * 失敗的添加一個元素僅僅拋出一個未檢查異常
  * <tt>offer</tt> method is designed for use when failure is a normal,
+ * offer方法設計成當通常的錯誤,而不是發生異常
  * rather than exceptional occurrence, for example, in fixed-capacity
+ *                                     例如，在合適的容量的隊列
  * (or &quot;bounded&quot;) queues.
  *
  * <p>The {@link #remove()} and {@link #poll()} methods remove and
+ * remove與poll方法移除和返回隊列的頭
  * return the head of the queue.
  * Exactly which element is removed from the queue is a
+ * 從隊列移除的元素恰好是一個隊列排序政策的不同實現的方法
  * function of the queue's ordering policy, which differs from
  * implementation to implementation. The <tt>remove()</tt> and
  * <tt>poll()</tt> methods differ only in their behavior when the
+ * remove,poll方法當隊列為空時行為不同
  * queue is empty: the <tt>remove()</tt> method throws an exception,
+ *                 remove拋出一個異常
  * while the <tt>poll()</tt> method returns <tt>null</tt>.
+ * poll返回null
  *
  * <p>The {@link #element()} and {@link #peek()} methods return, but do
+ * element與peek方法返回隊列的頭，但不移除
  * not remove, the head of the queue.
  *
  * <p>The <tt>Queue</tt> interface does not define the <i>blocking queue
+ * 隊列的接口未定義阻塞隊列方法
  * methods</i>, which are common in concurrent programming.  These methods,
+ *              這些方法是通用的在並發程序中
  * which wait for elements to appear or for space to become available, are
+ * 等待元素出現成為可用的空間
  * defined in the {@link java.util.concurrent.BlockingQueue} interface, which
+ * 這些方法在BlockingQueue接口中定義，擴展這個接口,
  * extends this interface.
  *
  * <p><tt>Queue</tt> implementations generally do not allow insertion
+ * queue實現一般運行插入null元素，
  * of <tt>null</tt> elements, although some implementations, such as
+ *                            儘管一些實現
  * {@link java.util.LinkedList}, do not prohibit insertion of <tt>null</tt>.
+ * 例如LinkedList,                不阻止插入null
  * Even in the implementations that permit it, <tt>null</tt> should
+ * 每一個實現都允許，null不應插入queue
  * not be inserted into a <tt>Queue</tt>, as <tt>null</tt> is also
  * used as a special return value by the <tt>poll</tt> method to
+ *  因為null也應用於一個特殊的有poll方法表示的隊列包含的元素的返回值
  * indicate that the queue contains no elements.
  *
  * <p><tt>Queue</tt> implementations generally do not define
+ * queue實現一般不定義equals與hashCode方法的基本版本
  * element-based versions of methods <tt>equals</tt> and
  * <tt>hashCode</tt> but instead inherit the identity based versions
+ * 但會用Object基本的版本替代表示
  * from class <tt>Object</tt>, because element-based equality is not
  * always well-defined for queues with the same elements but different
+ * 因為基本元素同樣的不允許定義好的，相同元素但不同排序屬性的隊列
  * ordering properties.
  *
  *
@@ -117,12 +156,14 @@ package z.z.w.jdk.collections;
  * @author Doug Lea
  * @param <E> the type of elements held in this collection
  */
-public interface Queue<E> extends java.util.Collection<E>
+public interface Queue<E> extends Collection<E>
 {
 	/**
 	 * Inserts the specified element into this queue if it is possible to do so
+	 * 當沒有違反容量限制立刻插入指定元素到隊列中
 	 * immediately without violating capacity restrictions, returning
 	 * <tt>true</tt> upon success and throwing an <tt>IllegalStateException</tt>
+	 * 返回true表示成功，如果當前沒有可用的空間，拋出一個異常
 	 * if no space is currently available.
 	 *
 	 * @param e the element to add
@@ -142,7 +183,9 @@ public interface Queue<E> extends java.util.Collection<E>
 	 * Inserts the specified element into this queue if it is possible to do
 	 * so immediately without violating capacity restrictions.
 	 * When using a capacity-restricted queue, this method is generally
+	 * 當使用容量受限的隊列，
 	 * preferable to {@link #add}, which can fail to insert an element only
+	 * 這個方法是通常最好比使用add,   他可以錯誤的添加一個元素僅僅由拋出的異常
 	 * by throwing an exception.
 	 *
 	 * @param e the element to add
@@ -159,7 +202,9 @@ public interface Queue<E> extends java.util.Collection<E>
 
 	/**
 	 * Retrieves and removes the head of this queue.  This method differs
+	 * 檢索
 	 * from {@link #poll poll} only in that it throws an exception if this
+	 * 這個方法與poll不同的地方僅僅在隊列為空時拋出一個異常
 	 * queue is empty.
 	 *
 	 * @return the head of this queue
@@ -170,6 +215,7 @@ public interface Queue<E> extends java.util.Collection<E>
 	/**
 	 * Retrieves and removes the head of this queue,
 	 * or returns <tt>null</tt> if this queue is empty.
+	 * 如果隊列為null返回null
 	 *
 	 * @return the head of this queue, or <tt>null</tt> if this queue is empty
 	 */
@@ -177,7 +223,9 @@ public interface Queue<E> extends java.util.Collection<E>
 
 	/**
 	 * Retrieves, but does not remove, the head of this queue.  This method
+	 * 檢索,      但不刪除,
 	 * differs from {@link #peek peek} only in that it throws an exception
+	 * 這個方法與peek的不同之處僅在 如果隊列為null拋出一個異常
 	 * if this queue is empty.
 	 *
 	 * @return the head of this queue
@@ -188,6 +236,7 @@ public interface Queue<E> extends java.util.Collection<E>
 	/**
 	 * Retrieves, but does not remove, the head of this queue,
 	 * or returns <tt>null</tt> if this queue is empty.
+	 * 如果隊列為null返回null
 	 *
 	 * @return the head of this queue, or <tt>null</tt> if this queue is empty
 	 */
